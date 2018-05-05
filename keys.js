8e715fc1d3b081e08c802ec637c7c9e8d260cf22
matt babcock <babcockmatthew12@yahoo.com>
2018-05-05 10:34:55 -0700

    buckle up bois

---
+++ keys.js
@@ -0,0 +1,12 @@
+exports.twitter = {
+    consumer_key: process.env.mv4dfhjOliqMaHSpR9WIzR9uo,
+    consumer_secret: process.env.uCNIvqchW1BnuKhnqqHhVCZaES7MdxRNtZ30kYKxIatNJXkBPz,
+    access_token_key: process.env.uRJrsDaTqrMkAQbCYMDkZ7zlnlU7Gvz,
+    access_token_secret: process.env.	cXbaOdv9gOT0ev3T4z4DDlfE9L8fzzl2KrpHs7vG7gg63
+  };
+
+  exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+  };
+  
