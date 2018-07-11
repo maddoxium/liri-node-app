d270d1997f002051178d67c778512b0a62b3ffad
matt babcock <babcockmatthew12@yahoo.com>
2018-07-11 14:37:09 -0700

    done

--- keys.js
+++ keys.js
@@ -1,12 +1,13 @@
+console.log('this is loaded');
+
 exports.twitter = {
     consumer_key: process.env.mv4dfhjOliqMaHSpR9WIzR9uo,
     consumer_secret: process.env.uCNIvqchW1BnuKhnqqHhVCZaES7MdxRNtZ30kYKxIatNJXkBPz,
     access_token_key: process.env.uRJrsDaTqrMkAQbCYMDkZ7zlnlU7Gvz,
-    access_token_secret: process.env.	cXbaOdv9gOT0ev3T4z4DDlfE9L8fzzl2KrpHs7vG7gg63
+    access_token_secret: process.env.cXbaOdv9gOT0ev3T4z4DDlfE9L8fzzl2KrpHs7vG7gg63
   };
 
   exports.spotify = {
-    id: process.env.SPOTIFY_ID,
-    secret: process.env.SPOTIFY_SECRET
+    id: process.env.8cd5a568fa934b558c19ef3c59fbc388,
+    secret: process.env.624b2dee8fe44d22b641ee3667cfb3e2
   };
-  
